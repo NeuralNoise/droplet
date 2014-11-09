1a8ee2b4a1484c946c992ca8420741fc4faea83d
José Antonio Calvo <jacalvo@zentyal.com>
2014-11-09 23:08:19 +0100

    added /status API with changed attribute

---
+++ droplet/rest/status/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from droplet import common
+
+class StatusViewSet(viewsets.ViewSet):
+    """
+    API endpoint that
+    """
+    def list(self, request):
+        return Response({'changed': common.changed()})
--- droplet/rest/urls.py
+++ droplet/rest/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework import routers
+from droplet.rest.status.views import StatusViewSet
 from droplet.rest.samba.views import DomainSettingsViewSet
 from droplet.rest.catalog.views import ModuleInfoViewSet
 
 router = routers.DefaultRouter()
 
+router.register(r'status', StatusViewSet, base_name='status')
 router.register(r'catalog/modules', ModuleInfoViewSet, base_name='catalog-modules')
 router.register(r'samba/settings', DomainSettingsViewSet)
 
